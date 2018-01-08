ccf982ebea924683005eb3316637b95366eaf2d8
MetaColon <timo@teemze.de>
2018-01-08 22:07:38 +0100

    Cleanup & ignoring missing packages

--- libfprint/drivers/elan.h
+++ libfprint/drivers/elan.h
@@ -18,6 +18,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "ConstExpressionRequired"
+#pragma ide diagnostic ignored "CannotResolve"
 #ifndef __ELAN_H
 #define __ELAN_H
 
@@ -46,57 +49,58 @@
 #define ELAN_CMD_TIMEOUT 10000
 #define ELAN_FINGER_TIMEOUT 200
 
-struct elan_cmd {
-	unsigned char cmd[ELAN_CMD_LEN];
-	int response_len;
-	int response_in;
+struct elan_cmd
+{
+    unsigned char cmd[ELAN_CMD_LEN];
+    int response_len;
+    int response_in;
 };
 
 static const struct elan_cmd get_sensor_dim_cmds[] = {
-	{
-	 .cmd = {0x00, 0x0c},
-	 .response_len = 0x4,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        {
+                .cmd = {0x00, 0x0c},
+                .response_len = 0x4,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static const size_t get_sensor_dim_cmds_len =
-array_n_elements(get_sensor_dim_cmds);
+        array_n_elements(get_sensor_dim_cmds);
 
 static const struct elan_cmd init_start_cmds[] = {
-	{
-	 .cmd = {0x40, 0x19},
-	 .response_len = 0x2,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
-	{
-	 .cmd = {0x40, 0x2a},
-	 .response_len = 0x2,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        {
+                .cmd = {0x40, 0x19},
+                .response_len = 0x2,
+                .response_in = ELAN_EP_CMD_IN,
+        },
+        {
+                .cmd = {0x40, 0x2a},
+                .response_len = 0x2,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static const size_t init_start_cmds_len = array_n_elements(init_start_cmds);
 
 static const struct elan_cmd read_cmds[] = {
-	/* raw frame sizes are calculated from image dimesions reported by the
-	 * device */
-	{
-	 .cmd = {0x00, 0x09},
-	 .response_len = -1,
-	 .response_in = ELAN_EP_IMG_IN,
-	 },
+        /* raw frame sizes are calculated from image dimesions reported by the
+         * device */
+        {
+                .cmd = {0x00, 0x09},
+                .response_len = -1,
+                .response_in = ELAN_EP_IMG_IN,
+        },
 };
 
 const size_t read_cmds_len = array_n_elements(read_cmds);
 
 /* issued after data reads during init and calibration */
 static const struct elan_cmd init_end_cmds[] = {
-	{
-	 .cmd = {0x40, 0x24},
-	 .response_len = 0x2,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        {
+                .cmd = {0x40, 0x24},
+                .response_len = 0x2,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static const size_t init_end_cmds_len = array_n_elements(init_end_cmds);
@@ -105,72 +109,76 @@ static const size_t init_end_cmds_len = array_n_elements(init_end_cmds);
  * original driver may observe return value to determine how many times it
  * should be repeated */
 static const struct elan_cmd calibrate_start_cmds[] = {
-	{
-	 .cmd = {0x40, 0x23},
-	 .response_len = 0x1,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
-	{
-	 .cmd = {0x40, 0x23},
-	 .response_len = 0x1,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        {
+                .cmd = {0x40, 0x23},
+                .response_len = 0x1,
+                .response_in = ELAN_EP_CMD_IN,
+        },
+        {
+                .cmd = {0x40, 0x23},
+                .response_len = 0x1,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static const size_t calibrate_start_cmds_len =
-array_n_elements(calibrate_start_cmds);
+        array_n_elements(calibrate_start_cmds);
 
 /* issued after data reads during init and calibration */
 static const struct elan_cmd calibrate_end_cmds[] = {
-	{
-	 .cmd = {0x40, 0x24},
-	 .response_len = 0x2,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        {
+                .cmd = {0x40, 0x24},
+                .response_len = 0x2,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static const size_t calibrate_end_cmds_len =
-array_n_elements(calibrate_end_cmds);
+        array_n_elements(calibrate_end_cmds);
 
 static const struct elan_cmd capture_start_cmds[] = {
-	/* led on */
-	{
-	 .cmd = {0x40, 0x31},
-	 .response_len = 0x0,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        /* led on */
+        {
+                .cmd = {0x40, 0x31},
+                .response_len = 0x0,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static size_t capture_start_cmds_len = array_n_elements(capture_start_cmds);
 
 static const struct elan_cmd capture_wait_finger_cmds[] = {
-	/* wait for finger
-	 * subsequent read will not complete until finger is placed on the reader */
-	{
-	 .cmd = {0x40, 0x3f},
-	 .response_len = 0x1,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        /* wait for finger
+         * subsequent read will not complete until finger is placed on the reader */
+        {
+                .cmd = {0x40, 0x3f},
+                .response_len = 0x1,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static size_t capture_wait_finger_cmds_len =
-array_n_elements(capture_wait_finger_cmds);
+        array_n_elements(capture_wait_finger_cmds);
 
 static const struct elan_cmd deactivate_cmds[] = {
-	/* led off */
-	{
-	 .cmd = {0x00, 0x0b},
-	 .response_len = 0x0,
-	 .response_in = ELAN_EP_CMD_IN,
-	 },
+        /* led off */
+        {
+                .cmd = {0x00, 0x0b},
+                .response_len = 0x0,
+                .response_in = ELAN_EP_CMD_IN,
+        },
 };
 
 static const size_t deactivate_cmds_len = array_n_elements(deactivate_cmds);
 
 static void elan_cmd_cb(struct libusb_transfer *transfer);
+
 static void elan_cmd_read(struct fpi_ssm *ssm);
+
 static void elan_run_next_cmd(struct fpi_ssm *ssm);
 
 static void elan_capture(struct fp_img_dev *dev);
 
 #endif
+
+#pragma clang diagnostic pop
