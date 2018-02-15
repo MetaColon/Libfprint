2951dd6b66a46b1bb0709bccbf33cf0ba4cfef06
metacolon <timo@teemze.de>
2018-02-15 17:56:22 +0100

    Logging and Fixing

--- ImageCapture/Main.py
+++ ImageCapture/Main.py
@@ -4,47 +4,56 @@ VENDOR_ID = 0x04f3
 PRODUCT_ID = 0x0c1a
 INTERFACE = 0
 WIDTH = 144
-HEIGHT = 64 #validate
+HEIGHT = 64  # validate
 USB_OUT = 0x0
 USB_IN = 0x80
 CMD_OUT = 0x1 | USB_OUT
 CMD_IN = 0x3 | USB_IN
 IMG_IN = 0x2 | USB_IN
-PRE_INIT = 0x4019
-INIT = 0x4031
-GET_IMAGE = 0x0009
-GET_PRE_SCAN = 0x403f
-STOP = 0x000b
+PRE_INIT = bytes([0x40, 0x19])
+INIT = bytes([0x40, 0x31])
+GET_IMAGE = bytes([0x00, 0x09])
+GET_PRE_SCAN = bytes([0x40, 0x3f])
+STOP = bytes([0x00, 0x0b])
 
 
 def init(h):
     h.bulkWrite(CMD_OUT, PRE_INIT)
+    print("Wrote PreInit")
     h.bulkWrite(CMD_OUT, INIT)
+    print("Wrote Init")
     return
 
 
 def getPreScan(h):
-    h.bulkWrite(CMD_OUT, )
+    h.bulkWrite(CMD_OUT, GET_PRE_SCAN)
+    print("Wrote GetPreScan")
     return
 
 
 def readPreScan(h):
+    print("Began to read prescan")
     res = h.bulkRead(CMD_IN, 1)
-    return res[0]#TODO Array? -> How to receive data?
+    print("Read PreScan")
+    return res[0]  # TODO Array? -> How to receive data?
 
 
 def getImage(h):
     h.bulkWrite(CMD_OUT, GET_IMAGE)
+    print("Wrote GetImage")
     return
 
 
 def readImage(h):
+    print("Image size: " + str(WIDTH * HEIGHT))
     res = h.bulkRead(IMG_IN, WIDTH * HEIGHT)
-    return res#TODO What is res?
+    print("Read image: " + str(res))
+    return res  # TODO What is res?
 
 
 def stop(h):
     h.bulkWrite(CMD_OUT, STOP)
+    print("Wrote stop")
     return
 
 
@@ -58,18 +67,21 @@ with usb1.USBContext() as context:
         print("Couldn't connect to device")
         exit(1)
     with handle.claimInterface(INTERFACE):
+        print("Got handle")
         index = 0
         while True:
             init(handle)
             getPreScan(handle)
-            if readPreScan(handle) != 0x55:
-                continue
+            #if readPreScan(handle) != 0x55:
+            #    continue
             getImage(handle)
             img = readImage(handle)
-            with open('image' + str(index) + '.pgm', 'wb') as f:#todo file ending
-                f.write(img)#validate
+            with open('image' + str(index) + '.pgm', 'wb') as f:  # todo file ending
+                print("Wrote image to " + str(f))
+                f.write(img)  # validate
 
             if input("Continue? (y/n):") != "y":
                 break
+            index = index + 1
         stop(handle)
         exit(0)
